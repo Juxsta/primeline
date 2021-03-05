f631d8d6b6e7e9ac1bcec805b7de012ff9df1beb
juxsta <ericjrreyes2@gmail.com>
2021-03-05 01:27:29 -0800

    darkened red

--- web/src/components/Footer/Footer.js
+++ web/src/components/Footer/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
               href="tel:850-123-5021"
               aria-label="Primeline Concrete Plus Global Phone Number"
               title="Primeline Concrete Plus Global Phone Numbe"
-              className="transition-colors duration-300 text-red-600 hover:text-deep-purple-800"
+              className="transition-colors duration-300 text-red-800 hover:text-deep-purple-800"
             >
               0997 436 9830
             </a>
@@ -40,7 +40,7 @@ const Footer = () => {
               href="tel:850-123-5021"
               aria-label="Primeline Concrete Plus Global Phone Number"
               title="Primeline Concrete Plus Global Phone Numbe"
-              className="transition-colors duration-300 text-red-600 hover:text-deep-purple-800"
+              className="transition-colors duration-300 text-red-800 hover:text-deep-purple-800"
             >
               0963 691 4380
             </a>
@@ -51,7 +51,7 @@ const Footer = () => {
               href="mailto:info@lorem.mail"
               aria-label="Our email"
               title="Our email"
-              className="transition-colors duration-300 text-red-600 hover:text-deep-purple-800"
+              className="transition-colors duration-300 text-red-800 hover:text-deep-purple-800"
             >
               ericsreyes0808@outlook.com
             </a>
@@ -64,7 +64,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               aria-label="Our address"
               title="Our address"
-              className="transition-colors duration-300 text-red-600 hover:text-deep-purple-800"
+              className="transition-colors duration-300 text-red-800 hover:text-deep-purple-800"
             >
               Sitio Libutad, Sta. Maria, Mabalacat City, Pampanga
             </a>
@@ -77,7 +77,7 @@ const Footer = () => {
           <div className="flex items-center mt-1 space-x-3">
             <a
               href="/"
-              className="text-gray-800 transition-colors duration-300 hover:text-red-600"
+              className="text-gray-800 transition-colors duration-300 hover:text-red-800"
             >
               <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                 <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"></path>
@@ -85,7 +85,7 @@ const Footer = () => {
             </a>
             <a
               href="/"
-              className="text-gray-800 transition-colors duration-300 hover:text-red-600"
+              className="text-gray-800 transition-colors duration-300 hover:text-red-800"
             >
               <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
                 <circle cx="15" cy="15" r="4"></circle>
@@ -94,7 +94,7 @@ const Footer = () => {
             </a>
             <a
               href="/"
-              className="text-gray-800 transition-colors duration-300 hover:text-red-600"
+              className="text-gray-800 transition-colors duration-300 hover:text-red-800"
             >
               <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                 <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"></path>
@@ -111,7 +111,7 @@ const Footer = () => {
           <li>
             <a
               href="/"
-              className="text-sm text-gray-800 transition-colors duration-300 hover:text-red-600"
+              className="text-sm text-gray-800 transition-colors duration-300 hover:text-red-800"
             >
               F.A.Q
             </a>
@@ -119,7 +119,7 @@ const Footer = () => {
           <li>
             <a
               href="/"
-              className="text-sm text-gray-800 transition-colors duration-300 hover:text-red-600"
+              className="text-sm text-gray-800 transition-colors duration-300 hover:text-red-800"
             >
               Privacy Policy
             </a>
@@ -127,7 +127,7 @@ const Footer = () => {
           <li>
             <a
               href="/"
-              className="text-sm text-gray-800 transition-colors duration-300 hover:text-red-600"
+              className="text-sm text-gray-800 transition-colors duration-300 hover:text-red-800"
             >
               Terms &amp; Conditions
             </a>
